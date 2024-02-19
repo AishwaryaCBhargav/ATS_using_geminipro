e634ad56e8b04f68d616187b7f2cb473e14081e9
aishwarya.chandra <aishwarya.chandra@currenciesdirect.com>
2024-02-19 14:22:32 +0000

    updated prompt

--- chatpdf1.py
+++ chatpdf1.py
@@ -42,7 +42,7 @@ def get_conversational_chain():
     prompt_template = """
     Hi, I will upload one or more pdf files and I need your help in understanding its content.
     Please assist me in extracting the information from the PDF files and provide insights and 
-    analysis based on the content of the PDF files.
+    analysis based on the content of the PDF files. Provide a detailed answer for every question asked.
     Always provide complete sentences that are terminated by a period (full-stop).
     The answer should only be in the context of the uploaded PDF files.
     If the answer is not in the provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
