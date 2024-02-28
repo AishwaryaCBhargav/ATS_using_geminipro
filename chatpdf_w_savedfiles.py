c755024606f57249599a40fcca0acf3a456fce93
aishwarya.chandra <aishwarya.chandra@currenciesdirect.com>
2024-02-28 14:32:31 +0000

    added .stramlit folder

--- chatpdf_w_savedfiles.py
+++ chatpdf_w_savedfiles.py
@@ -109,7 +109,8 @@ def main():
     with st.sidebar:
         st.title("Menu:")
         uploaded_files = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
-        if st.button("Submit & Process"):
+        if st.button("Submit & Process"; background-color: #4CAF50;
+            color: white):
             with st.spinner("Processing..."):
                 pdf_paths = save_uploaded_files(uploaded_files)
                 raw_text = get_pdf_text(pdf_paths)
