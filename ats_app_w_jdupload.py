3aea6f1413a4b5dab7baa9dae3e1b15e61ef6f40
aishwarya.chandra <aishwarya.chandra@currenciesdirect.com>
2024-03-13 15:52:19 +0000

    ats_w_jdupload

--- ats_app_w_jdupload.py
+++ ats_app_w_jdupload.py
@@ -29,41 +29,46 @@ def input_pdf_text(uploaded_file):
     
 ## Streamlit App
 
-st.set_page_config(page_title="ATS Resume EXpert")
-st.header("ATS Tracking System")
-# input_text=st.text_area("Job Description: ",key="input")
-# uploaded_file=st.file_uploader("Upload your resume(PDF)...",type=["pdf"])
+def main():
+    st.set_page_config(page_title="ATS Resume EXpert")
+    st.header("ATS Tracking System")
+    # input_text=st.text_area("Job Description: ",key="input")
+    # uploaded_file=st.file_uploader("Upload your resume(PDF)...",type=["pdf"])
 
-st.subheader('Upload Job Description PDF')
-job_description_file = st.file_uploader('Upload JD file', type='pdf')
+    st.subheader('Upload Job Description PDF')
+    job_description_file = st.file_uploader('Upload JD file', type='pdf')
 
-st.subheader('Upload Resume PDF')
-resume_file = st.file_uploader('Upload Resume file', type='pdf')
-
-
-if job_description_file and resume_file:
-    st.write("PDFs Uploaded Successfully")
+    st.subheader('Upload Resume PDF')
+    resume_file = st.file_uploader('Upload Resume file', type='pdf')
 
 
-submit1 = st.button("Tell Me About the Resume")
-
-
-#Input prompt for the first submit button
-input_prompt1 = """
- You are an experienced Technical Human Resource Manager,your task is to review the provided resume against the job description. 
- Please share your professional evaluation on whether the candidate's profile aligns with the role. 
- Highlight the strengths and weaknesses of the applicant in relation to the specified job requirements.
-"""
-
-
-if submit1:
     if job_description_file and resume_file:
-        job_desc_text = input_pdf_text(job_description_file)
-        resume_text = input_pdf_text(resume_file)
+        st.write("PDFs Uploaded Successfully")
 
-        response=get_gemini_response(input_prompt1,resume_text,job_desc_text)
-        st.subheader("The Repsonse is")
-        st.write(response)
-    else:
-        st.write("Please upload the resume")
+
+    submit1 = st.button("Tell Me About the Resume")
+
+
+    #Input prompt for the first submit button
+    input_prompt1 = """
+    You are an experienced Technical Human Resource Manager,your task is to review the provided resume against the job description. 
+    Please share your professional evaluation on whether the candidate's profile aligns with the role. 
+    Highlight the strengths and weaknesses of the applicant in relation to the specified job requirements.
+    """
+
+
+    if submit1:
+        if job_description_file and resume_file:
+            job_desc_text = input_pdf_text(job_description_file)
+            resume_text = input_pdf_text(resume_file)
+
+            response=get_gemini_response(input_prompt1,resume_text,job_desc_text)
+            st.subheader("The Repsonse is")
+            st.write(response)
+        else:
+            st.write("Please upload the resume")
+
+ 
+if __name__ == "__main__":
+    main()
 
